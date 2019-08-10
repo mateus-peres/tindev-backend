7e70ac535bcd2e4e3c8d21b8101cd59f3a72fe41
Mateus Peres <mateuspsouza@gmail.com>
2019-08-10 12:19:03 -0300

    Versão inicial da API TinDev

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-qubtr.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json())
+server.use(routes);
+
+
+server.listen(3333);
